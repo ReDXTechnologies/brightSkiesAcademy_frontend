c6b9413b2977b982d535ef9e98250aa19de0503f
imenchaari2 <imen.chaari@enis.tn>
2023-05-06 17:54:43 +0100

    apiUrl:"https://brightskiesacademy.com/api"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:"http://brightskiesacademy-1715150313.us-east-1.elb.amazonaws.com/api"
+  // apiUrl:"http://brightskiesacademy-1715150313.us-east-1.elb.amazonaws.com/api"
+  apiUrl:"https://brightskiesacademy.com/api"
 };
